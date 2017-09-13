794a9a5b680d9ea254e46f625aa8d94d8a252fb6
Nicola Bertoldi <nicolabertoldi@Nicolas-MacBook-Pro.local>
2017-09-13 12:17:51 +0200

    added script for attaching a new disk

---
+++ Attach_Disk.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+
+set -e
+
+# -----------------------------------
+# This script attaches a new disk (Standard_LRS, 512Gb) to an existing VM 
+#
+# Please update variables
+#   destGroup
+#   destSize
+#   destDiskType
+# to meet your requirements
+# -----------------------------------
+
+destGroup=ROL-test
+
+# -----------------
+# default variables
+#
+user=hlt_admin
+passwd=HltMtUser2017
+
+# -----------------
+# derived variables
+#
+destVmName=ROL-test-a
+destDiskName="$destVmName-DataDisk"
+destDiskSize="512"
+destDiskType="Standard_LRS"
+
+# 0) attache a new disk to the specified VM
+args="--resource-group $destGroup --vm-name $destVmName"
+args="$args --disk $destDiskName"
+args="$args --size-gb $destDiskSize"
+args="$args --sku $destDiskType"
+args="$args --new"
+az vm disk attach $args
+
+echo exit code $?
+
